ba9bb806994145f110a79de2d0f220dac0f05fe9
chizo <filip.cierkosz@gmail.com>
2022-01-13 21:36:32 +0100

    The Factorial directory is up-to-date.

---
+++ Factorial/factorial.py
@@ -0,0 +1,18 @@
+# Written by: Filip J. Cierkosz
+# Date: 07/2021
+
+import sys
+
+# The default recursion is about 1000. It can be set higher manually.
+sys.setrecursionlimit(4000)
+
+# Function to recursively get the factioral of a number.
+def factorial(n):
+    if (n==0 or n==1): return 1
+    elif (n<0): return None
+    else: return n*factorial(n-1)
+
+# Test harness.
+# A separate datafile is created to write the result of the factorial
+# function, since the number might be huge.
+print(factorial(3000), file=open("factorialWrite.txt", "w"))
