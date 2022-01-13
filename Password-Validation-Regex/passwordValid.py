c18b4ccf27cf90892476ccd1977cc05b6ea72ab6
chizo <filip.cierkosz@gmail.com>
2022-01-13 20:45:31 +0100

    The Password-Validation-Regex has new files and is up-to-date.

---
+++ Password-Validation-Regex/passwordValid.py
@@ -0,0 +1,25 @@
+# Written by: Filip J. Cierkosz
+# Date: 07/2021
+
+import re
+
+# Function to validate a password using regular expression.
+# The password has to be at least 6-character long, with at least
+# one uppercase letter, one lowercase letter, and one digit.
+# It cannot contain any special signs.
+def validPassword(pswrd):
+    # Define the regular expression.
+    regex = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])[A-Za-z0-9]{6,}$"
+
+    # Check if the input matches the pattern.
+    matches = re.findall(regex, pswrd)
+
+    # If there are any matches, the password is valid.
+    if (len(matches)==1): return True
+    
+    # Otherwise, the input password is not valid.
+    return False
+
+# Test harness.
+print(validPassword('aTest01')) # True
+print(validPassword('invalidp@ssword')) # False
