95871d8e8fc1984cf8af18df59a70ee7d0d4839d
chizo <filip.cierkosz@gmail.com>
2022-01-13 21:38:02 +0100

    The file has been updated.

--- Factorial/factorial.py
+++ Factorial/factorial.py
@@ -8,8 +8,8 @@ sys.setrecursionlimit(4000)
 
 # Function to recursively get the factioral of a number.
 def factorial(n):
-    if (n==0 or n==1): return 1
-    elif (n<0): return None
+    if (n<0): return None
+    elif (n==0 or n==1): return 1
     else: return n*factorial(n-1)
 
 # Test harness.
