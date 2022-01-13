2f24c34bd36170d1559c0c05d4cd5601ecfd543a
chizo <filip.cierkosz@gmail.com>
2022-01-13 20:35:22 +0100

    Character-Counter directory is up-to-date.

---
+++ Character-Counter/charCounter.py
@@ -0,0 +1,11 @@
+# Written by: Filip J. Cierkosz
+# Date: 11/2021
+
+# Function to count the number of a character in a string 
+# implementing lambda and dictionary comprehension.
+count = lambda s: {c: s.count(c) for c in s.replace(' ', '')}
+
+# Test harness.
+print(count('this is just a test'))
+print(count("knjsvksv sjdjnvikjvns vjnsf vksnv snjfmv skjvjfsnvf"))
+print(count('aaaaaBBBaaaaaaaCCCCCCaaa'))
