b0cf71bb4756686979d6617a17bf82465000a158
chizo <filip.cierkosz@gmail.com>
2022-01-14 12:51:24 +0100

    The file has been updated.

---
+++ Addition-Binding-Function/bindingAddition.py
@@ -0,0 +1,15 @@
+# Written by: Filip J. Cierkosz
+# Date: 06/2021
+
+from functools import partial
+
+# Addition of 5 numbers implementing a binding function.
+def add(a,b,c,d,e):
+    return a+b+c+d+e
+
+# Test harness.
+addOne = partial(add, 1)
+addTwo = partial(addOne, 2)
+addThree = partial(addTwo, 3)
+addFour = partial(addThree, 4)
+print(addFour(5))
