262270ae9cad98b2dd23b873b2af40cb6aed280c
chizo <filip.cierkosz@gmail.com>
2022-01-25 13:37:18 +0000

    The final version of the file has been added.

--- Snail-Sort/snailSort.py
+++ Snail-Sort/snailSort.py
@@ -7,17 +7,14 @@ import numpy as np
 # returns a modified version of the input array so that it is arranged from
 # outermost elements to the middle element, traveling clockwise.
 def snail(arrIn):
-    # If the size of the input array is 0 or 1, return an array 
-    # holding an empty array.
-    if (len(arrIn)==0 or len(arrIn)==1):
-        return [arrIn]
-    # Otherwise, perform the snail algortihm.
+    if (len(arrIn)<2):
+        return arrIn[0]
     else:
-        # Initialize the array with an appropriate size.
+        # Initialize an appropriately-sized array.
         arrSnail = np.empty(int(len(arrIn[0])**2), dtype=int)
         currIndex = 0
 
-        # Initialize the idexes of columns and rows to append elements from.
+        # Initialize the idexes of columns and rows.
         rowUp = 0
         colRight = len(arrIn[0])-1
         rowBottom = len(arrIn)-1
@@ -27,13 +24,12 @@ def snail(arrIn):
         # the index of the right column, and the upper row does not exceed
         # the index of the bottom row. The rules are straightforward - 
         # retrieve the values following the order while iteration:
-        # upper row->right column->bottom row->left column (then, repeat).
+        # upper row->right column->bottom row->left column (if needed, repeat).
         while (rowUp<=rowBottom and colLeft<=colRight):
             # Append all the elements from the upper row of the array.
             for i in range(colLeft, colRight+1):
                 arrSnail[currIndex] = arrIn[rowUp][i]
                 currIndex += 1
-
             # Increment the row, i.e. go one row down.
             rowUp += 1
 
@@ -41,8 +37,7 @@ def snail(arrIn):
             for i in range(rowUp, rowBottom+1):
                 arrSnail[currIndex] = arrIn[i][colRight]
                 currIndex += 1 
-
-            # Decrement the column, i.e. go one column to the right.
+            # Decrement the column, i.e. go one column to the left.
             colRight -= 1
 
             # Append all the elements from the bottom row of the array.
@@ -50,7 +45,6 @@ def snail(arrIn):
                 for i in range(colRight, colLeft-1, -1):
                     arrSnail[currIndex] = arrIn[rowBottom][i]
                     currIndex += 1
-
             # Decrement the row, i.e. go one row up.
             rowBottom -= 1
 
@@ -59,17 +53,16 @@ def snail(arrIn):
                 for i in range(rowBottom, rowUp-1, -1):
                     arrSnail[currIndex] = arrIn[i][colLeft]
                     currIndex += 1
-
             # Increment the column, i.e. go one column to the right.
             colLeft += 1
 
         # Return the snail-sorted array.
-        return arrSnail.tolist()
+        return arrSnail
 
 # Test harness.
-arrZero = []
-arrOne = [1]
-arrTwo = [[1,2,3],[8,9,4],[7,6,5]]
+arrZero = [[]]
+arrOne = [[1]]
+arrTwo = np.reshape([1,2,3,8,9,4,7,6,5], (3,3))
 arrThree = np.reshape([i for i in range(1,26)], (5, 5))
 arrFour = np.reshape([i for i in range(1,82)], (9,9))
 
